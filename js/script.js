bbfa774cf35ce46aecacdff1b2881006b73242db
Amir Tavakoli <amirtavakoli@outlook.com>
2018-07-03 15:03:06 +0430

    fonts (icomoon, vazir) added

--- js/script.js
+++ js/script.js
@@ -1,112 +1,57 @@
-// fullpagejs
-$(document).ready(function() {
-  $('#fullpage').fullpage({
-  //Navigation
-  menu: '#menu',
-  lockAnchors: false,
-  anchors:['welcome', 'intro', 'thirdPage','contact'],
-  navigation: false,
-  navigationPosition: 'right',
-  navigationTooltips: ['firstSlide', 'secondSlide'],
-  showActiveTooltip: false,
-  slidesNavigation: false,
-  slidesNavPosition: 'bottom',
+$(document).ready(function(){
+    
+    
+  // ************** Slider ****************
+  // Options
+  var speed=500;
+  var autoswitch =true;
+  var autoswitch_speed=8000;
   
-  //Scrolling
-  css3: true,
-  scrollingSpeed: 700,
-  autoScrolling: true,
-  fitToSection: true,
-  fitToSectionDelay: 1000,
-  scrollBar: false,
-  easing: 'easeInOutCubic',
-  easingcss3: 'ease',
-  loopBottom: false,
-  loopTop: false,
-  loopHorizontal: true,
-  continuousVertical: false,
-  continuousHorizontal: false,
-  scrollHorizontally: false,
-  interlockedSlides: false,
-  dragAndMove: false,
-  offsetSections: false,
-  resetSliders: false,
-  fadingEffect: false,
-  normalScrollElements: '#element1, .element2',
-  scrollOverflow: false,
-  scrollOverflowReset: false,
-  scrollOverflowOptions: null,
-  touchSensitivity: 15,
-  normalScrollElementTouchThreshold: 5,
-  bigSectionsDestination: null,
+  $('.slide').first().addClass('active');
   
-  //Accessibility
-  keyboardScrolling: true,
-  animateAnchor: true,
-  recordHistory: true,
+  // Hide All Slides 
+  $('.slide').hide();
   
-  //Design
-  controlArrows: true,
-  verticalCentered: true,
-  sectionsColor : ['#fff', '#4addff','#fff'],
-  paddingTop: '3em',
-  paddingBottom: '10px',
-  fixedElements: '#header, .footer',
-  responsiveWidth: 0,
-  responsiveHeight: 0,
-  responsiveSlides: false,
-  parallax: false,
-  parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+  // Show First Slide
+  $('.active').show();
   
-  //Custom selectors
-  sectionSelector: '.section',
-  slideSelector: '.slide',
+  // Next Handler
+  $('#next').on('click',NextSlide);
   
-  lazyLoading: false,
+  // previous Handler
+  $('#prev').on('click',PrevSlide);
   
-  //events
-  onLeave: function(origin, destination, direction){},
-  afterLoad: function(origin, destination, direction){},
-  afterRender: function(){},
-  afterResize: function(){width, height},
-  afterResponsive: function(isResponsive){},
-  afterSlideLoad: function(section, origin, destination, direction){},
-  onSlideLeave: function(section, origin, destination, direction){}
-  });
+  // Switch to Next
+  function NextSlide(){
+      $('.active').removeClass('active').addClass('oldActive');
+      if($('.oldActive').is(':last-child')){
+          $('.slide').first().addClass('active');
+      } else{
+          $('.oldActive').next().addClass('active');
+      }
+      $('.oldActive').removeClass('oldActive');
+      $('.slide').fadeOut(speed);
+      $('.active').fadeIn(speed);
+  }
   
-  //methods
-  $.fn.fullpage.setAllowScrolling(true);
+  // Switch to Previous
+  function PrevSlide(){
+      $('.active').removeClass('active').addClass('oldActive');
+      if($('.oldActive').is(':first-child')){
+          $('.slide').last().addClass('active');
+      } else{
+          $('.oldActive').prev().addClass('active');
+      }
+      $('.oldActive').removeClass('oldActive');
+      $('.slide').fadeOut(speed);
+      $('.active').fadeIn(speed);
+  }
+  
+  
+  // Auto Slider Handler
+  if(autoswitch==true){
+      setInterval(NextSlide,autoswitch_speed);
+  }
+  
+  
 });
-
-
-// typewriter
-$(document).ready(() => {  
-    // Initialize Typewriter effect for Our Slogans
-    let typewriter = new Typewriter(document.getElementById('fields'), {
-      loop: true,
-      typingSpeed: 50,
-      deleteSpeed: 10,
-      cursor: ''
-    });
-  
-    // Typewriter Sequence
-    typewriter.typeString('IoT')
-        .pauseFor(2500)
-        .deleteAll()
-        .typeString('Artificial Intelligence')
-        .pauseFor(2500)
-        .deleteAll()
-        .typeString('Machine Learning')
-        .pauseFor(2500)
-        .deleteAll()
-        .typeString('Big Data')
-        .pauseFor(2500)
-        .deleteAll()
-        .typeString('Cloud Computing')
-        .pauseFor(2500)
-        .deleteAll()
-        .typeString('Data Processing')
-        .pauseFor(2500)
-        .deleteAll()
-        .start();
-  });
